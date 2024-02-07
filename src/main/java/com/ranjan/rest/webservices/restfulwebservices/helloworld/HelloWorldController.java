55d37c6ae51638c101418d62039c57c865a31d89
Ravi Ranjan <raviraz73@gmail.com>
2024-02-07 09:43:19 +0530

    Hello  World Bean for JSON response

--- src/main/java/com/ranjan/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/ranjan/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -12,4 +12,8 @@ public class HelloWorldController {
 		return "Hello World";
 	}
 
+	@GetMapping(path="hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
 }
