2389035a38786466660a7ac2df4c541b3d860e7c
Ravi Ranjan <raviraz73@gmail.com>
2024-02-13 21:15:53 +0530

    Implementing DELETE Method to delete a User Resource

--- src/main/java/com/ranjan/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/ranjan/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,15 @@ public class UserResource {
 		User savedUser = userDaoService.save(user);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		return ResponseEntity.created(location ).build();
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User retrieveUser = userDaoService.retrieveUser(id);
+		if(retrieveUser == null)
+			throw new UserNotFoundException("No such user to delete");
+		else
+			userDaoService.deleteUser(retrieveUser.getId());
 	}
 
 }
